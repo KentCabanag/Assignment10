a75ebf254ad6cb567001d90da52b261fc6267ff8
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2022-02-17 15:46:58 +0800

    commit

--- Program1(PLD10).py
+++ Program1(PLD10).py
@@ -15,3 +15,23 @@
 	#- Create a demo of your program (1-2 min) and send it directly to my messenger.
 
 import cv2
+import webbrowser
+
+from cv2 import QRCodeDetector
+
+cap = cv2.VideoCapture(0)
+detector = cv2.QRCodeDetector()
+
+while True:
+	_, img = cap.read()
+	data, bbox, _ = detector.detectAndDecode(img)
+	if data:
+		a=data
+		break
+	cv2.imshow("QRCODEscanner", img)
+	if cv2.waitKey(1) == ord("q"):
+		break
+
+b=webbrowser.open(str(a))
+cap.release()
+cv2.destroyAllWindows()
